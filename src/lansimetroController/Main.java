5fe4c832a5789cb6402c95404b01c6d4ba8fcc03
Iepvzaeh <thehirwi@gmail.com>
2018-02-28 15:36:48 +0200

    Added image
    Added image lansimetro.png

--- src/lansimetroController/Main.java
+++ src/lansimetroController/Main.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -21,8 +22,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-        
-        Scene scene = new Scene(root);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(root);
+         scrollPane.setPrefSize(583.0,700);
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Länsimetro Simulator 3000");
         stage.setScene(scene);
         stage.show();
